bcc2f06303a6a75413b59a8e00a3d424c8c47d17
shubhamgandhi3112 <132562844+shubhamgandhi3112@users.noreply.github.com>
2023-10-19 12:35:25 +0530

    Add files via upload

---
+++ analog clock/analog clock/index.js
@@ -0,0 +1,15 @@
+function updateClock() {
+    const d = new Date();
+    const htime = d.getHours();
+    const mtime = d.getMinutes();
+    const stime = d.getSeconds();
+    const hrotation = 30 * htime + mtime / 2;
+    const mrotation = 6 * mtime;
+    const srotation = 6 * stime;
+
+    document.getElementById("Hour").style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById("Minute").style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById("Second").style.transform = `rotate(${srotation}deg)`;
+}
+
+setInterval(updateClock, 1000);
